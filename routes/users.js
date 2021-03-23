44a8bb037a064648c45b76144b3442bbde24f6ca
Bassem <bassem.fayed@gmail.com>
2021-03-23 15:16:11 +0100

    test heroku 2

--- routes/users.js
+++ routes/users.js
@@ -173,7 +173,7 @@ router.get('/myProfile/:token', async function (req, res, next) {
   // rdv futurs : il y a un bouton qui amène vers myDetails
   // rdv passés : il y a un bouton qui amène vers comment
 
-
+console.log(req.params.token)
   //A priori le populate ne sert plus après changement de méthode
   const tokenUser = req.params.token;
   const user = await UserModel.findOne({ token: tokenUser })
