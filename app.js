afe5240b5f929adc3f5cce777155da3350a1b31c
Bassem <bassem.fayed@gmail.com>
2021-03-23 10:36:15 +0100

    moved build to express

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'reactapp/build')));
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'reactapp/build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
