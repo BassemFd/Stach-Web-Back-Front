ed08248914621759b208df0aa07924efb66c79e6
Bassem <bassem.fayed@gmail.com>
2021-03-23 10:30:09 +0100

    heroku 2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'reactapp/build', 'index.html'));
 });
 
